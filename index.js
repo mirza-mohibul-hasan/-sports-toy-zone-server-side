e1cf18ac15db0c32f5fcfcabc38eca335135e8f2
Mirza Mohibul Hasan <mirzamohibul618@.com>
2023-05-19 01:27:05 +0600

    DB USER and PASS Added using env

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
@@ -8,7 +9,7 @@ app.use(cors())
 app.use(express.json())
 
 // MongoDB
-const uri = `mongodb+srv://**:**@cluster0.clbkfrr.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.clbkfrr.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
