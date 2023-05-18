1e06709c2cd204e6fb8f82f808e58e5478b02a85
Mirza Mohibul Hasan <mirzamohibul618@.com>
2023-05-19 01:23:20 +0600

    Mongo Setup added

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const cors = require('cors')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const app = express()
+const port = process.env.PORT || 5000;
+// Middleware
+app.use(cors())
+app.use(express.json())
+
+// MongoDB
+const uri = `mongodb+srv://**:**@cluster0.clbkfrr.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    /* Working Place Start */
+    
+    /* Working Place End */
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+// Default
+app.get('/', (req, res) => {
+  res.send('Sports Toy server is running!')
+})
+app.listen(port, () => {
+  console.log(`Sports Toy server is listening on port ${port}`)
+})
