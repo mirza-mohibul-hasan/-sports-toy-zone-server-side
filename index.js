e0ebdad1b36c49f4293d59946f5c2389598b7489
Mirza Mohibul Hasan <mirzamohibul618@.com>
2023-05-19 10:21:43 +0600

    My toys with email added

--- index.js
+++ index.js
@@ -41,6 +41,15 @@ async function run() {
         const result = await toysCollection.find(query).toArray()
         res.send(result)
     })
+    // Get My toys
+    app.get('/mytoys', async(req, res)=>{
+        let query = {}
+        if (req.query?.email) {
+          query = { sellerEmail: req.query.email }
+        }
+          const result = await toysCollection.find(query).toArray()
+          res.send(result)
+    })
 
     /* Working Place End */
     // Send a ping to confirm a successful connection
